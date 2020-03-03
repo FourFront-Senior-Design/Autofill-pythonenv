d45d6e605a9d3d139e8c527aaef3086e473f5443
Jay Donohue <jay.donohue@mines.sdsmt.edu>
2020-03-03 01:39:58 -0700

    Removed args_to_file tests after args_to_file function was removed.

--- FourFrontScripts/tests/test_dateExtraction.py
+++ FourFrontScripts/tests/test_dateExtraction.py
@@ -4,27 +4,6 @@ import dateExtraction
 import json
 
 
-def test_args_to_file_list1():
-    """args_to_file_list should return list of single file name"""
-    file1 = "C:\\SomeSection\\GoogleVisionData\\some_file.json"
-    args = (file1, )
-    files = dateExtraction.args_to_file_list(args)
-    file_list = list()
-    file_list.append(file1)
-    assert files == file_list
-
-
-def test_args_to_file_list2():
-    """args_to_file_list should return list of file names"""
-    file1 = "C:\\SomeSection\\GoogleVisionData\\some_file1.json"
-    file2 = "C:\\SomeSection\\GoogleVisionData\\some_file2.json"
-    args = (file1 + " " + file2, )
-    files = dateExtraction.args_to_file_list(args)
-    file_list = list()
-    file_list.append(file1)
-    file_list.append(file2)
-    assert files == file_list
-
 ### TODO(jd): Need tests for get_json_data function
 
 ### TODO(jd): Need tests for get_text_annotations function
