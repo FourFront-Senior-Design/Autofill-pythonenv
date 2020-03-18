37917676bca8861713b2e46044c3d1dd04e6cef0
Morgan Benninghofen <morgan.benninghofen@mines.sdsmt.edu>
2020-03-18 17:44:23 -0500

    Added rank extraction module and one test for basic functionality. More tests to come...

--- FourFrontScripts/Extraction/rankExtraction.py
+++ FourFrontScripts/Extraction/rankExtraction.py
@@ -1,6 +1,12 @@
-from DataStructures import ranks
+from DataAccess.OCR_Data import OCR_Data
+from DataAccess.OCR_Data import Word
+from DataStructures.ranks import ranks
 
-def extractRanks(filePath, data):
+'''
+Creates a map of Rank field to rank values that matches the database and fills that map with
+ranks found in the given data object.
+'''
+def extractRanks(data):
     rankMap = {
     'Rank': '',
     'Rank2': '',
@@ -13,13 +19,33 @@ def extractRanks(filePath, data):
     'RankS_D_4': ''
     }
     
-    # Naive approach: Search the ranks array for every word in the string less than the max rank size (6)
+    # Naive approach: Search the ranks map for every word in the string less than the max rank size (6)
+
+    # Get all words from data object
+    frontWordList, backWordList = data.getWords()
     
-    # Split string by spaces
+    rankList = list()
     
-    # For each word less than 6 characters long, search ranks array
-    
-        # Insert into return array
+    # For each word
+    for w in frontWordList + backWordList:
+
+        # If the word is less than the max length of a rank (6)
+        # and it matches a rank from the rank list
+        if w.text in ranks and len(w.text) < 6:
+            # Insert into list
+            rankList.append(w.text)
+            
+    # Add ranks to rankMap
+    rankMap["Rank"] = None if len(rankList) < 1 else rankList[0]
+    rankMap["Rank2"] = None if len(rankList) < 2 else rankList[1]
+    rankMap["Rank3"] = None if len(rankList) < 3 else rankList[2]
+    rankMap["RankS_D"] = None if len(rankList) < 4 else rankList[3]
+    rankMap["Rank2S_D"] = None if len(rankList) < 5 else rankList[4]
+    rankMap["Rank3S_D"] = None if len(rankList) < 6 else rankList[5]
+    rankMap["RankS_D_2"] = None if len(rankList) < 7 else rankList[6]
+    rankMap["RankS_D_3"] = None if len(rankList) < 8 else rankList[7]
+    rankMap["RankS_D_4"] = None if len(rankList) < 9 else rankList[8]
+        
         
     # return all ranks found
-    pass
+    return rankMap
