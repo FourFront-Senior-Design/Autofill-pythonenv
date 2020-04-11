098d8136d58ad6aa02fc82e29f7d0f68b0c4e2a1
Morgan Benninghofen <32240897+mjbenninghofen@users.noreply.github.com>
2020-04-10 19:29:53 -0500

    Invert method by searching fullText for strings
    instead of trying to match words to wars

--- FourFrontScripts/Extraction/warExtraction.py
+++ FourFrontScripts/Extraction/warExtraction.py
@@ -2,6 +2,8 @@ from DataAccess.OCR_Data import OCR_Data
 from DataAccess.OCR_Data import Word
 from DataStructures.wars import wars
 
+import re
+
 '''
 Creates a map of war field to war values that matches the database and fills that map with
 wars found in the given data object.
@@ -21,19 +23,19 @@ def extractWars(data):
     'WarS_D_4': ''
     }
     
-    # Naive approach: Search the war map for every word in the string
+    # Naive approach: Search the string for every war
 
     # Get all words from data object
-    frontWordList, backWordList = data.getWords()
+    fullText = data.getFullText()
     
     warList = list()
     
     # For each word
-    for w in frontWordList + backWordList:
-        # If the word matches a war from the war list
-        if w.text in wars:
+    for w in wars:
+        # If the key matches a with the string
+        if re.match(w, fullText):
             # Insert into list
-            warList.append(wars[w.text])
+            warList.append(wars[w])
             
     # Add wars to warMap
     warMap["War"] = "" if len(warList) < 1 else warList[0]
